e8fd83104141467bb3f42412aea8e881f9b95e83
ntammana <n_tammana@u.pacific.edu>
2015-05-27 15:30:39 -0700

    testing

--- myscriptGIT.bash
+++ myscriptGIT.bash
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-#bash script to add, commit and push files in a directory
 git add .
-read -p "Commit message:" desc
+read -p "Commit message: " desc
 git commit -m "$desc"
-git push origin master
+git push origin master 
