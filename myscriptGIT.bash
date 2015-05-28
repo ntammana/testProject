b26ea46d1996e0a86aa400a2159fd6f99f77ff78
ntammana <n_tammana@u.pacific.edu>
2015-05-28 15:40:43 -0700

    testing

--- myscriptGIT.bash
+++ myscriptGIT.bash
@@ -3,4 +3,11 @@
 git add --all .
 read -p "Commit message: " desc
 git commit -m "$desc"
-git push origin master 
+git push origin master
+
+cd ./testProject
+for f in *
+do
+	cp -v -r $f ../testProjectSVN/
+done
+ 
