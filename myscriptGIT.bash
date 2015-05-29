528c098f6dc6660e583adc0222a8c063ba9aac0f
ntammana <n_tammana@u.pacific.edu>
2015-05-29 13:43:04 -0700

    testing

--- myscriptGIT.bash
+++ myscriptGIT.bash
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-git add --all .
-read -p "Commit message: " desc
-git commit -m "$desc"
-git push origin master
-
+read -p "Ready to push to git? (y/n)" gitanswer
+if [ $gitanswer = y ]
+then
+{
+	git add --all .
+	read -p "Commit message: " desc
+	git commit -m "$desc"
+	git push origin master
+}
+else 
+{
+	exit
+}
+fi
+echo "moving project to local svn directory"
 cd ./testProject
 for f in *
 do
@@ -12,8 +22,18 @@ do
 done
 
 #bash script to add commit and push files into svn directory
-cd ~/testProjectSVN/nttestproject
-svn update
-svn add * --force
+read -p "Ready to commit and push into svn directory? (y/n)" svnanswer
+if [$svnanswer == "y"]
+then
+{
+	cd ~/testProjectSVN/nttestproject
+	svn update
+	svn add * --force
 
-svn commit -m "$desc" 
+	svn commit -m "$desc" 
+}
+else
+{
+	exit
+}
+fi
