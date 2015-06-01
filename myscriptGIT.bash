52ebca1af0554a407f2320ef3b4c8e7964d59e1c
ntammana <n_tammana@u.pacific.edu>
2015-06-01 12:12:31 -0700

    testing 2

--- myscriptGIT.bash
+++ myscriptGIT.bash
@@ -16,7 +16,7 @@ else
 fi
 echo "moving project to local svn directory"
 #cd ./testProject
-rm -rf ../testProjectSVN/nttestproject
+rm -rf ~/testProjectSVN/nttestproject
 for f in *
 do
 	cp -r $f ../testProjectSVN/nttestproject
