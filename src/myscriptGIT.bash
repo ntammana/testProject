6de33290a0024dab52bd0463b228549eaf09e9d0
ntammana <n_tammana@u.pacific.edu>
2015-06-03 11:47:11 -0700

    nothing

--- src/myscriptGIT.bash
+++ src/myscriptGIT.bash
@@ -28,6 +28,7 @@ read -p "Ready to commit and push into svn directory? (y/n)" svnanswer
 if [ $svnanswer = y ]
 then
 {
+	svn delete -r ~/nttestproject/src
 	cd ~/nttestproject/src
 	svn update
 	svn add * --force
