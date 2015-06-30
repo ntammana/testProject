85896e1edd104015b3da1b5febaad91714f1ed87
ntammana <n_tammana@u.pacific.edu>
2015-06-30 11:47:23 -0700

    ok

--- src/BooleanToken.java
+++
@@ -1,7 +0,0 @@
-//changes for merge history
-class BooleanToken extends Token 
-{
-	public BooleanToken(boolean_value) { value = _value;}
-	public boolean getValue() {return value;}
-	private boolean value;
-};
